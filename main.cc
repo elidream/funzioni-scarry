9da80c89676f19d7e8aa9cbc7c91c32f6032ce69
elidream <114856807+elidream@users.noreply.github.com>
2022-10-05 18:47:50 +0200

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <sstream> 
+#include <fstream>
+#include "funzioni.h"
+using namespace std;
+
+
+int main(){
+
+    stringstream sout;   //my printer
+    int ntot = 0;
+    ...*w = load_data(".dat", ntot);
+
+    
+    print_and_save(sout);
+
+    delete [] w;
+
+    return 0;
+}
